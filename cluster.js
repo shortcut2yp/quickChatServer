3f8e30c72417d794d92fc612fc9940de1c630d1d
shortcut2yp <yashpatel001@gmail.com>
2021-08-07 09:19:34 +0100

    updated cluster.js

--- cluster.js
+++ cluster.js
@@ -1,31 +1,35 @@
 import cluster from "cluster"
 import http from "http"
+import { cpus } from "os"
 import process from "process"
 import { setupMaster } from "@socket.io/sticky"
 import { start } from "./index.js"
 
-const WORKERS_COUNT = 4
+const WORKERS_COUNT = 10
+
 if (cluster.isMaster) {
   console.log(`Master ${process.pid} is running`)
-  for (let i = 0; i < WORKERS_COUNT.length; i++) {
+
+  for (let i = 0; i < WORKERS_COUNT; i++) {
     cluster.fork()
   }
 
   cluster.on("exit", (worker) => {
-    console.log(`Worker ${worker.process.pid} is dead !`)
+    console.log(`Worker ${worker.process.pid} died!`)
     cluster.fork()
   })
 
   const httpServer = http.createServer()
   setupMaster(httpServer, {
-    loadBalancingMethod: "least-connection", //random , round-robin
+    loadBalancingMethod: "least-connection", // random, robin-robin
   })
 
   const PORT = process.env.PORT || 4000
-  httpServer.listen(4000, () => {
-    console.log(`Server listening at port ${PORT}`)
+
+  httpServer.listen(PORT, () => {
+    console.log(`Server listening at port${PORT}`)
   })
 } else {
-  console.log(`Worker ${process.pid} started !`)
+  console.log(`Worker ${process.pid} started`)
   start()
 }
